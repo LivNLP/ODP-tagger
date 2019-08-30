e927b09c5de2bac253cdc169e1ad3c7608e127b8
michealabaho <michealabaho265@gmail.com>
2019-08-30 17:59:16 +0100

    learning from batches and introducing glove

--- seq_models/outcome_model.py
+++ seq_models/outcome_model.py
@@ -6,43 +6,85 @@ from torch import optim
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class outcomeEncoder(nn.Module):
-    def __init__(self, embedding_dim, hidden_size, vocab_size, n_layers=1):
+    def __init__(self, embedding_dim, hidden_size, vocab_size, bidirectional=False):
         super(outcomeEncoder, self).__init__()
         self.hidden_size = hidden_size
-        self.n_layers = n_layers
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_size)
-
-        #self.map2output = nn.Linear(hidden_size, num_classes)
+        if bidirectional:
+            self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size, bidirectional=True)
+        else:
+            self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size)
 
     def forward(self, input, hidden):
         c_hidden = hidden
         embedding = self.embeddings(input).view(1, 1, -1)
         output, enc_hidden = self.lstm(embedding, (hidden, c_hidden))
-        #tag_space = self.map2output(lstm_out.view(len(input), -1))
-        #tag_scores = F.log_softmax(tag_space, dim=1)
         return output, enc_hidden[0]
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
 class outcomeDecoder(nn.Module):
-    def __init__(self, hidden_size, output_size):
+    def __init__(self, hidden_size, output_size, dropout, n_layers, bidirectional=False):
         super(outcomeDecoder, self).__init__()
         self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(dropout)
         self.embedding = nn.Embedding(output_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size)
+        if bidirectional:
+            self.lstm = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=n_layers, bidirectional=True)
+        else:
+            self.lstm = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=n_layers)
         self.out = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden):
-        c_hidden = hidden
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
-        output, dec_hidden = self.lstm(output, (hidden, c_hidden))
+        d_hidden = hidden
+        input_embedding = self.embedding(input).view(1, 1, -1)
+        input_embedding = self.dropout(input_embedding)
+        input_embedding = F.relu(input_embedding)
+        output, dec_hidden = self.lstm(input_embedding, (hidden, d_hidden))
         output = self.softmax(self.out(output[0]))
-        return output, dec_hidden[0]
+        return output, dec_hidden
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class outcomeAttnDecoder(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout, n_layers, max_length):
+        super(outcomeAttnDecoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout = nn.Dropout(dropout)
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.attn = nn.Linear(hidden_size*2, max_length)
+        self.attn_combine = nn.Linear(hidden_size*2, hidden_size)
+        self.lstm = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=n_layers)
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        ad_hidden = hidden
+        embedded = self.embedding(input).view(1, 1, -1)
+        input_embedding = self.dropout(embedded)
+
+        input_transformed = self.attn(torch.cat((embedded[0], hidden[0]), 1))
+        attn_weights = F.softmax(input_transformed, dim=1)
+
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0), encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, dec_hidden = self.lstm(output, (hidden, ad_hidden))
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+
+        return output, dec_hidden, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
         
--- seq_models/outcome_train.py
+++ seq_models/outcome_train.py
@@ -1,27 +1,20 @@
 import torch
 import torch.nn as nn
-from seq_models import outcome_model as model
+import sys
+sys.path.append('/users/phd/micheala/Documents/Github/Health-outcome-tagger/')
+import outcome_model as model
 import torch.optim as optim
-import data_prep as utils
+import data_prep as data_loader
+import helper_functions as utils
 import time
+import numpy as np
+import os
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+start_outcome_token, end_outcome_token = 0, 1
 
-def train(epochs, learning_rate, print_every, use_teacher_forcing=False):
-    file_path = 'ebm-data/train_ebm.bmes'
-    word_map, word_count, index2word = utils.create_vocabularly(file_path)
-    line_pairs, outputs = utils.readwordTag(file_path)
-    encoded_outputs = utils.create_tag_map(outputs)
-    input_words, output_tags, max_len = utils.inputAndOutput(line_pairs, word_map, encoded_outputs)
-
-    encoder = model.outcomeEncoder(embedding_dim=50,
-                                   hidden_size=100,
-                                   vocab_size=len(word_map),
-                                   n_layers=1).to(device)
-
-    decoder = model.outcomeDecoder(hidden_size=100,
-                                   output_size=len(outputs)).to(device)
-
+def train(encoder, decoder, epochs, input_words, output_tags, max_len, learning_rate, print_every, batch_size, use_teacher_forcing=False):
     losses = []
     print_loss_total = 0
     plot_loss_total = 0
@@ -31,54 +24,176 @@ def train(epochs, learning_rate, print_every, use_teacher_forcing=False):
     decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
 
     loss_criterion = nn.NLLLoss()
+    start = time.time()
+    epoch_loss = 0
+    for epoch in range(epochs):
+        training_tensors = list(zip(input_words, output_tags))
+        epoch_batch_loss,epoch_batch_acc  = 0, 0
+        for batch_pairs in utils.batch_generator(training_tensors, batch_size):
+            batch_loss, batch_acc  = 0, 0
+            for batch_tensor in batch_pairs:
+                input_tensor = batch_tensor[0].to(device)
+                target_tensor = batch_tensor[1].to(device)
+                encoder_optimizer.zero_grad()
+                decoder_optimizer.zero_grad()
 
-    for iter in range(epochs):
-        start = time.time()
-        input_tensor = input_words[iter]
-        target_tensor = output_tags[iter]
+                input_len, target_len = input_tensor.size(0), target_tensor.size(0)
 
-        encoder_optimizer.zero_grad()
-        decoder_optimizer.zero_grad()
+                encoder_hidden = encoder.initHidden()
+                encoder_outputs = torch.zeros(max_len, encoder.hidden_size, device=device)
 
-        input_len, target_len = input_tensor.size(0), target_tensor.size(0)
+                loss = 0
 
-        encoder_hidden = encoder.initHidden()
-        encoder_outputs = torch.zeros(max_len, encoder.hidden_size, device=device)
+                for e in range(input_len):
+                    encoder_output, out_encoder_hidden = encoder(input_tensor[e], encoder_hidden)
+                    encoder_outputs[e] = encoder_output[0, 0]
 
-        loss = 0
+                decoder_hidden = out_encoder_hidden
+                decoder_input = torch.tensor([[start_outcome_token]], device=device)
+                predicted_tags = []
+                if use_teacher_forcing:
+                    for d in range(target_len):
+                        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden, encoder_outputs)
+                        loss += loss_criterion(decoder_output, target_tensor[d])
+                        decoder_input = target_tensor[d]
 
-        for e in range(input_len):
-            encoded_output, out_encoder_hidden = encoder(input_tensor[e], encoder_hidden)
-            encoded_outputs[e] = encoded_output[0, 0]
+                else:
+                    for d in range(target_len):
+                        decoder_output, out_decoder_hidden, attn_weights = decoder(decoder_input, decoder_hidden, encoder_outputs)
+                        topv, topi = decoder_output.topk(1)
+                        decoder_input = topi.squeeze().detach()
+                        loss += loss_criterion(decoder_output, target_tensor[d])
+                        if decoder_input.item() == end_outcome_token:
+                            break
+                        else:
+                            predicted_tags.append(topi.item())
+                loss.backward()
 
-        decoder_hidden = out_encoder_hidden
+                encoder_optimizer.step()
+                decoder_optimizer.step()
 
-        if use_teacher_forcing:
-            for d in range(target_len):
-                decoder_output, decoder_hidden = decoder(target_tensor[d], decoder_hidden)
-                loss += loss_criterion(decoder_output, target_tensor[d])
+                #loss
+                batch_loss += (loss.item()/target_len)
 
+                #accuracy
+                true_tags = [i.item() for i in target_tensor][:-1]
+                predicted_tags = predicted_tags[1:]
+                sim, tot = utils.token_level_accuracy(true_tags, predicted_tags)
+                batch_acc += sim/tot
+            epoch_batch_loss += (batch_loss/len(batch_pairs))
+            epoch_batch_acc = (batch_acc/len(batch_pairs))
+            print("AVERAGE BATCH_LOSS {} AVERAGE TOKEN LEVEL ACCURACY {}".format((batch_loss/len(batch_pairs)), (epoch_batch_acc)))
+        epoch_loss += (epoch_batch_loss/batch_size)
+        losses.append(epoch_batch_loss/batch_size)
+        print('EPOCH_LOSS {}'.format(epoch_batch_loss/batch_size))
+        if (epoch+1) % print_every == 0:
+            now = time.time()
+            print('Average Loss after {} epochs is {},  Duration - {:.4f}'.format((epoch+1), (epoch_loss/print_every), (now-start)))
+            epoch_loss = 0
+
+
+def evaluate(encoder, decoder, input_words, embedding_dim, hidden_size, output_tags, max_len, word_map, index2word):
+    input_length = input_words.size(0)
+    encoder_hidden = encoder.initHidden()
+
+    encoder_outputs = torch.zeros(max_len, encoder.hidden_size, device=device)
+    loss_criterion = nn.NLLLoss()
+
+    loss = 0
+
+    for u in range(input_length):
+        encoded_output, encoder_hidden = encoder(input_words[u], encoder_hidden)
+        encoder_outputs[u] += encoded_output[0, 0]
+
+    decoder_hidden = encoder_hidden
+    decoded_tags = []
+
+    loss_criterion = nn.NLLLoss()
+
+    decoder_input = torch.tensor([[start_outcome_token]])
+    for v in range(max_len):
+        decoder_output, dec_hidden = decoder(decoder_input, decoder_hidden)
+        topv, topi = decoder_output.data.topk(1)
+        decoded_tags.append(index2word[topi.item()])
+        if v < input_length:
+            loss += loss_criterion(decoder_output, output_tags[v])
+            decoder_input = topi.squeeze().detach()
         else:
-            decoder_input = target_tensor[0]
-            for d in range(target_len):
-                decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-                topv, topi = decoder_output.topk(1)
-                decoder_input = topi.squeeze().detach()
-                loss += loss_criterion(decoder_output, target_tensor[d])
+            break
+    return decoded_tags, loss.item()
 
-        loss.backward()
+def load_and_process(file_path, evaluation=False):
+    word_map, word_count, index2word = data_loader.create_vocabularly(file_path)
+    line_pairs, outputs = data_loader.readwordTag(file_path)
+    tag_map = data_loader.create_tag_map(outputs)
+    input_words, output_tags, max_len = data_loader.inputAndOutput(line_pairs, word_map, tag_map)
+    if evaluation:
+        return word_map, index2word, line_pairs, outputs, input_words, output_tags, max_len
+    else:
+        return line_pairs, word_map, index2word, outputs, input_words, output_tags, max_len
 
-        encoder_optimizer.step()
-        decoder_optimizer.step()
 
-        print_loss_total += loss
+if __name__=='__main__':
+    #training
+    file_path = 'ebm-data/train_ebm.bmes'
+    linepairs, word_map, index2word, outputs, input_words, output_tags, max_len = load_and_process(file_path)
 
-        #if (iter) % print_every == 0:
-        now = time.time()
-        print('Average Loss {} Duration - {:.4f}'.format((print_loss_total), (now-start)))
+    embedding_dim = 50
+    hidden_size = 100
+    dropout=0.1
 
-        return loss.item()/target_len
+    encoder = model.outcomeEncoder(embedding_dim=embedding_dim,
+                                   hidden_size=hidden_size,
+                                   vocab_size=len(word_map)).to(device)
 
-train(epochs=10, learning_rate=0.001, print_every=2)
+    decoder = model.outcomeDecoder(hidden_size=hidden_size,
+                                   output_size=len(outputs),
+                                   dropout=dropout,
+                                   n_layers=1).to(device)
+
+    attndecoder = model.outcomeAttnDecoder(hidden_size=hidden_size,
+                                           output_size=len(outputs),
+                                           dropout=dropout,
+                                           n_layers=1,
+                                           max_length=max_len).to(device)
+
+    train(encoder=encoder,
+          decoder=attndecoder,
+          input_words=input_words,
+          output_tags=output_tags,
+          max_len=max_len,
+          epochs=100,
+          batch_size=375,
+          learning_rate=0.001,
+          print_every=5)
+
+
+    # #testing
+    #
+    # w, i, l, o, input_test_words, output_test_tags, max_l = load_and_process("ebm-data/test_ebm.bmes", evaluation=True)
+    # testing_loss = []
+    # with open('decoded.out', 'w') as dec_out:
+    #     for x,y,z in zip(input_test_words, output_test_tags, l):
+    #         predicted, loss = evaluate(encoder=encoder,
+    #                  decoder=decoder,
+    #                  input_words=x,
+    #                  embedding_dim=embedding_dim,
+    #                  hidden_size=hidden_size,
+    #                  output_tags=y,
+    #                  max_len=max_len,
+    #                  word_map=word_map,
+    #                  index2word=index2word)
+    #
+    #         dec_out.writelines(predicted)
+    #         dec_out.writelines(z[0])
+    #         dec_out.writelines('\n')
+    #         testing_loss.append(loss/len(i))
+    #     dec_out.close()
+    #
+    # print(np.mean(testing_loss))
+
+
+
+
 
 

dcf48d7d887b12e867589f1945dfd15b34f2a4b4
michealabaho <michealabaho265@gmail.com>
2019-12-13 14:05:48 +0000

    catering for the nested outcomes while extracting outcomes from the annotations

--- read_word_anns.py
+++ read_word_anns.py
@@ -9,31 +9,46 @@ from nltk.corpus import stopwords
 import string
 import spacy
 import os
+from pprint import pprint
 from tabulate import tabulate
 
 
 def extract_highlighted_words(files):
+    #pick only .docx files and create three seperate data frames outcomes, tools and highlighted words
     files = [i for i in files if i.endswith('.docx')]
     comet_data = {}
-    comet_data_frame = pd.DataFrame()
-    outcomes_frame = pd.DataFrame()
+    comet_outcomes_frame = pd.DataFrame()
+    highlights_frame = pd.DataFrame()
     tools_frame = pd.DataFrame()
+
     for file in files:
         doc = Document(os.path.abspath(file))
-        doc_content = doc.paragraphs
+        doc_content = doc.paragraphs #extract content from the word document
         doc_tag_list = []
-        outcomes = []
+        highlights = []
         tools = []
         for s,t in enumerate(doc_content):
             abs_text = t.text
+            #fetch highlighted words
             for v in fetch_highlights(t):
-                outcomes.append(v)
+                highlights.append(v)
+            #pick text annotated as tools
             picked_tools = pickup_tools(abs_text)
             if len(picked_tools) > 0:
                 for tool in picked_tools:
                     tools.append(tool)
+            #look through lines in the paragrpahs, identifying outcomes annotated by <P #>.....</>
             for ann in re.findall('(\<P\s[\d\,\s]+\>)(.+?(?=\<))(\<\/\>)', abs_text):
                 annot = ann[1]
+                #check nested outcomes outcomes <P #>..<P #>..<\>...</>, detected with closing tag <\>
+                if re.search('(\<P\s[\d\,\s]+\>)(.+?(?=\<))(\<\\\>)', annot):
+                    tag_no = re.findall(r'\d+', annotation[0])
+                    phrase = re.search('(\<P\s[\d\,\s]+\>)(.+?(?=\<))(\<\\\>)', annot)
+                    for tg in tag_no:
+                        doc_tag_list.append(('P {}'.format(tg), phrase.groups()[1].strip()))
+                    annot = re.sub(re.escape(phrase.group().strip()), '', annot)
+
+                #remove content in curly braces {}, because it's not needed in final outcome
                 brace_regex = re.findall('\{.+?\}', annot)
                 for _ in brace_regex:
                     f = re.compile(r'\{.+?\}')
@@ -42,66 +57,68 @@ def extract_highlighted_words(files):
                 ann[1] = ' '.join(annot.split())
                 annotation = [i.strip() for i in ann]
 
+                #dealing with precise outcomes, no additional criteria required to extract outcomes from within openning and closing tags of an outcome
                 if not re.search('\<P\s[\d\,\s]+\>', annotation[1]):
                     tag_no = re.findall(r'\d+', annotation[0])
                     phrase = annotation[1].strip(' ')
+                    phrase = remove_tools_from_outcomes(phrase)
                     if len(tag_no) == 1:
                         doc_tag_list.append(('P {}'.format(tag_no[0]), phrase))
                     elif len(tag_no) > 1:
                         for tg in tag_no:
                             doc_tag_list.append(('P {}'.format(tg), phrase))
-                else:
+
+                #dealing with outcomes that either share and ending or a start, additional criteria to process these outcomes
+                elif re.search('^\(E\d+\)', annotation[1].strip()) or re.search('^\(S\d+\)', annotation[1].strip()):
                     tags = re.findall('\s?\<P\s[\d\,\s]+\>\s?', annotation[1])
                     tags.insert(0, annotation[0])
                     tags = [i.strip(' <>') for i in tags]
+                    x = int(annotation[1][2])
+                    annotation_ = annotation[1][5:]
+
                     if re.search('^\(E\d+\)', annotation[1].strip()):
-                        x = int(annotation[1][2])
-                        annotation_ = annotation[1][5:]
                         phrases = re.split('\s*\<P\s[\d\,\s]+\>\s*', annotation_)
                         end = phrases[-1].split()
                         last_phrase = [i for i in end if i not in get_un_needed()]
                         for i in range(len(phrases)):
                             if i < len(phrases)-1:
                                 inner_phrase = ' '.join([i for i in phrases[i].split() if i not in get_un_needed()] + end[-(x):])
-                                # inner_phrase = re.sub('[\{\}\[\]\.]+', '', inner_phrase)
-                                #inner_phrase = [i for i in inner_phrase.split() if i not in get_un_needed()]
                                 inner_phrase = re.sub('\,','',inner_phrase)
                                 doc_tag_list.append((tags[i].strip(), inner_phrase))
                         last_phrase = ' '.join(last_phrase)
                         last_phrase = re.sub('\,', '', last_phrase)
                         doc_tag_list.append((tags[-1], last_phrase))
+
                     elif re.search('^\(S\d+\)', annotation[1].strip()):
-                        x = int(annotation[1][2])
-                        annotation_ = annotation[1][5:]
                         phrases = re.split('\s*\<P\s[\d\,\s]+\>\s*', annotation_)
                         start = [i for i in phrases[0].split() if i not in get_un_needed()]
                         for i in range(len(phrases)):
                             if i > 0:
                                 inner_phrase = ' '.join(start[:(x)] + [i for i in re.split(' |\,|\.', phrases[i]) if i not in get_un_needed()])
-                                # inner_phrase = re.sub('[\{\}\[\]\.]+', '', inner_phrase)
-                                # inner_phrase = [i for i in inner_phrase.split() if i not in get_un_needed()]
                                 inner_phrase = re.sub('\,', '', inner_phrase)
                                 doc_tag_list.append((tags[i], inner_phrase))
                         last_phrase = ' '.join(start)
                         last_phrase = re.sub('\,', '', last_phrase)
                         doc_tag_list.append((tags[0], last_phrase))
-                    else:
-                        print(annotation, file)
+                #any other outcomes
+                else:
+                    pass
+                    print(annotation, file)
         #dataframe_for_outcomes
-        x = pd.DataFrame(doc_tag_list)
+        x = pd.DataFrame(doc_tag_list, columns=['Labels', 'Outcomes'])
         x.index = [os.path.basename(file) for i in range(x.shape[0])]
-        comet_data_frame = pd.concat([comet_data_frame, x], axis=0)
+        comet_outcomes_frame = pd.concat([comet_outcomes_frame, x], axis=0)
 
-        y = pd.DataFrame(outcomes)
+        y = pd.DataFrame(highlights)
         y.index = [os.path.basename(file) for i in range(y.shape[0])]
-        outcomes_frame = pd.concat([outcomes_frame, y], axis=0)
+        highlights_frame = pd.concat([highlights_frame, y], axis=0)
 
         z = pd.DataFrame(tools)
         z.index = [os.path.basename(file) for i in range(z.shape[0])]
         tools_frame = pd.concat([tools_frame, z], axis=0)
 
-    comet_data_frame.to_csv('comet_data.csv')
-    outcomes_frame.to_csv('comet_outcomes.csv')
+    comet_outcomes_frame.to_csv('comet_outcomes.csv')
+    highlights_frame.to_csv('highlighted.csv')
     tools_frame.to_csv('tools.csv')
 
 def fetch_highlights(t):
@@ -137,13 +154,26 @@ def get_un_needed():
 def pickup_tools(t):
     tools = []
     for u in re.findall('\[T(.+?)\]', t):
-        for q in ['\<P\s[\d\,\s]+\>', '\<\/\>']:
+        for q in [r'\<P\s[\d\,\s]+\>', r'\<\/\>', r'\<\\>']:
             r = re.compile(q)
             g = r.findall(u)
             for un in g:
                 u = re.sub(un, '', u)
+        u = u.strip()
+        for q2 in ['^\(E\d+\)', '^\(S\d+\)']:
+            u = re.sub(q2, '', u)
+        u = re.sub(r'\<\\>', '', u)
+        if re.search('\,', u):
+            print(u)
         tools.append(' '.join(u.split()))
     return tools
+
+def remove_tools_from_outcomes(phrase):
+    if re.search('\[T(.+?)\]', phrase):
+        o = re.compile('\[T.+?\]')
+        phrase = o.sub('', phrase)
+    return ' '.join(phrase.split())
+
 
 def main():
     path = 'ebm-data/comet-data/'
@@ -154,4 +184,4 @@ def main():
 
 if __name__ == '__main__':
     print(os.getcwd())
-    #main()
+    main()
---
+++ seq_models/bert_embeddings.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Author: micheala
+# @Created: 11/12/19 
+# @Contact: michealabaho265@gmail.com
